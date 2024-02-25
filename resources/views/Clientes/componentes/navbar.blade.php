b45bb51fba793b35fc5eaa04d69155d122b390a0
DestroyerC7 <carlosfernandolopez12@gmail.com>
2024-02-25 00:31:40 -0400

    creacion de componentes para la barra de navegacion y el footer

---
+++ resources/views/Clientes/componentes/navbar.blade.php
@@ -0,0 +1,48 @@
+<header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li> <img src="{{ asset('imagenes/header logo.jpg') }}" alt="header"></li> <!-- Ajusta la ruta de la imagen -->
+                    <li class="nav-item"><a class="nav-link" id="INICIO" href="hindex.html">INICIO</a></li>
+                    <li class="nav-item">
+                    <a class="nav-link" id="NOSOTROS" href="{{ route('nosotros.nosotros') }}">NOSOTROS</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="SERVICIOS.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            SERVICIOS
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Soporte para impresoras</a>
+                            <a class="dropdown-item" href="#">Soporte de Ordenadores</a>
+                            <a class="dropdown-item" href="#">Otros</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            ARTICULOS
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                            <a class="dropdown-item" href="#">Artículo 1</a>
+                            <a class="dropdown-item" href="#">Artículo 2</a>
+                            <a class="dropdown-item" href="#">Artículo 3</a>
+                        </div>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" id="CONTACTANOS" href="#">CONTACTANOS</a></li>
+                    <li class="nav-item"><a class="nav-link" id="INGRESAR" href="">INGRESAR</a></li>
+                </ul>
+            </div>
+            <div class="loader">
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+                <div class="loader-square"></div>
+            </div>
+        </nav>
+</header>
