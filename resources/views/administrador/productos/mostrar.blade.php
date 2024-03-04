5a7ac0b984e2b3e68064c09b66385cbfb3f7aac2
JhoelDraco <peraltacruzjhoel@gmail.com>
2024-03-04 00:15:02 -0400

    restaurando cambios

--- resources/views/administrador/productos/mostrar.blade.php
+++ resources/views/administrador/productos/mostrar.blade.php
@@ -13,45 +13,45 @@
 
 
 <div class="container">
-    <div class="custom-card">
-        <div class="product-info">
-            <label for="detalleProducto" class="product-label">Codigo Del Producto:</label>
-            <p class="product-value">{{$producto->codigo}}</p>
+    <div class="custom-card1">
+        <div class="product-info1">
+            <label for="detalleProducto" class="product-label1">Codigo Del Producto:</label>
+            <p class="product-value1">{{$producto->codigo}}</p>
         </div>
     </div>
 
-    <div class="custom-card">
-        <div class="product-info">
-            <label for="detalleProducto" class="product-label">Nombre del Producto:</label>
-            <p class="product-value">{{$producto->nombre}}</p>
+    <div class="custom-card2">
+        <div class="product-info2">
+            <label for="detalleProducto" class="product-label2">Nombre del Producto:</label>
+            <p class="product-value2">{{$producto->nombre}}</p>
         </div>
     </div>
 
-    <div class="custom-card">
-        <div class="product-info">
-            <label for="detalleProducto" class="product-label">Detalle del Producto:</label>
-            <p class="product-value">{{$producto->detalle}}</p>
+    <div class="custom-card3">
+        <div class="product-info3">
+            <label for="detalleProducto" class="product-label3">Detalle del Producto:</label>
+            <p class="product-value3">{{$producto->detalle}}</p>
         </div>
     </div>
 
-    <div class="custom-card">
-        <div class="product-info">
-            <label for="detalleProducto" class="product-label">Precio del Producto:</label>
-            <p class="product-value">{{$producto->precio}}</p>
+    <div class="custom-card4">
+        <div class="product-info4">
+            <label for="detalleProducto" class="product-label4">Precio del Producto:</label>
+            <p class="product-value4">{{$producto->precio}}</p>
         </div>
     </div>
 
-    <div class="custom-card">
-        <div class="product-info">
-            <label for="detalleProducto" class="product-label">Stock del Producto:</label>
-            <p class="product-value">{{$producto->stock}}</p>
+    <div class="custom-card5">
+        <div class="product-info5">
+            <label for="detalleProducto" class="product-label5">Stock del Producto:</label>
+            <p class="product-value5">{{$producto->stock}}</p>
         </div>
     </div>
 
-    <div class="custom-card">
-        <div class="product-info">
-            <label for="detalleProducto" class="product-label">Imagen del producto:</label>
-            <img src="{{$producto->imagen}}" alt="Imagen del producto" class="product-image">
+    <div class="custom-card6">
+        <div class="product-info6">
+            <label for="detalleProducto" class="product-label6">Imagen del producto:</label>
+            <img src="{{$producto->imagen}}" alt="Imagen del producto" class="product-image6">
         </div>
     </div>
 </div>
